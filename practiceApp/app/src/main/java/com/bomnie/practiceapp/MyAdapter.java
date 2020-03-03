7a4bab28a3f96798fa9477ebe98213f3cdb88cfb
Bomin Kim <bomnie.kim@gmail.com>
2020-03-03 09:58:52 +0900

    news API 데이터를 위한 어댑터

--- practiceApp/app/src/main/java/com/bomnie/practiceapp/MyAdapter.java
+++ practiceApp/app/src/main/java/com/bomnie/practiceapp/MyAdapter.java
@@ -1,5 +1,7 @@
 package com.bomnie.practiceapp;
 
+import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +11,14 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.List;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private String[] mDataset;
+    private List<NewsData> mDataset;
+    private static View.OnClickListener onClickListener;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -21,22 +29,33 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         // each data item is just a string in this case
         public TextView tv_title;
         public TextView tv_content;
-        public ImageView iv_title;
+        public SimpleDraweeView iv_title;
+        public View rootView;
 
-        public MyViewHolder(View v) {
+        public MyViewHolder(View v) { // v: card_news.xml 의 root
             super(v);
+            // ViewHolder = 메모리 관리와 성능 향상을 위해 안드로이드에서 친절히 만들어준 클래스
             // card_news.xml의 자식요소들을 재사용할 수 있도록
             tv_title = v.findViewById(R.id.tv_title);
             tv_content = v.findViewById(R.id.tv_content);
             iv_title = v.findViewById(R.id.iv_title);
-            // ViewHolder = 메모리 관리와 성능 향상을 위해 안드로이드에서 친절히 만들어준 클래스
+            rootView = v;
+
+            v.setClickable(true); // card_news.xml(LinearLayout)을 클릭 가능하게끔
+            v.setEnabled(true);
+            v.setOnClickListener(onClickListener);
+            // 카드 하나의 어느 곳을 누르더라도 본문 내용으로 넘어가게 하도록 v에 설정
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
     // mDataset: 각 card_news 뷰마다 보여줄 값을 가지고 있는 초기 데이터 원본
-    public MyAdapter(String[] myDataset) {
+    public MyAdapter(List<NewsData> myDataset, Context context, View.OnClickListener onClick) {
         mDataset = myDataset;
+        onClickListener = onClick;
+        Fresco.initialize(context);
+
+        // context는 Activity에서 받아오는 것이기 때문에 단순한 class에서는 사용가
     }
 
     // Create new views (invoked by the layout manager)
@@ -57,13 +76,27 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.tv_title.setText(mDataset[position]);
+        NewsData news = mDataset.get(position);
+
+        holder.tv_title.setText(news.getTitle());
+        holder.tv_content.setText(news.getContent());
+
+        Uri uri = Uri.parse(news.getUrlToImage());
+        holder.iv_title.setImageURI(uri);
+
+        // 여기에 클릭이벤트를 달면 갱신될 때마다 클릭이벤트를 생성하기 때문에 비효율적
+        // tag - label
+        holder.rootView.setTag(position); // v에 태그를 다는 작업
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mDataset == null ? 0 : mDataset.size();
+    }
+
+    public NewsData getNews(int position){
+        return mDataset != null ? mDataset.get(position) : null ;
     }
 }
