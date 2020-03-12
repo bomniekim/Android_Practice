e90bfd79676f114ab400c0693685499d1325cbbd
Bomin Kim <bomnie.kim@gmail.com>
2020-03-12 21:55:38 +0900

    Firebase realtime database를 이용한 chatting app project

---
+++ ChattingApp/app/src/main/java/com/bomnie/chattingapp/ChattingActivity.java
@@ -0,0 +1,23 @@
+package com.bomnie.chattingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ChattingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatting);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
